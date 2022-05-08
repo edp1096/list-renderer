36411ce4d252dd8cc9f76ef7b36d62bd318c175a
edp1096 <edp1096@naver.com>
2022-05-08 21:08:22 +0900

    cjs, mjs

--- esbuild.build.js
+++ esbuild.build.js
@@ -3,13 +3,23 @@
 require('esbuild').build({
     entryPoints: ["ts/list-renderer.ts"],
     bundle: true,
-    // outdir: "dist",
+    outfile: "dist/list-renderer.js",
+    minify: true,
+    sourcemap: true,
+    target: "es2020",
+    format: "cjs",
+    define: { "process.env.NODE_ENV": "production" },
+    // define: { "process.env.NODE_ENV": "developemnt" },
+})
+
+require('esbuild').build({
+    entryPoints: ["ts/list-renderer.ts"],
+    bundle: true,
     outfile: "dist/list-renderer.mjs",
     minify: true,
     sourcemap: true,
     target: "es2020",
-    // format: "cjs",
     format: "esm",
-    // define: { "process.env.NODE_ENV": "production" }
-    define: { "process.env.NODE_ENV": "developemnt" }
+    define: { "process.env.NODE_ENV": "production" },
+    // define: { "process.env.NODE_ENV": "developemnt" },
 })
