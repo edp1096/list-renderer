a73f01d057234d705c7a58c34582e6f3bc00499b
edp1096 <edp1096@naver.com>
2022-05-17 11:18:08 +0900

    remove iife

--- esbuild.build.js
+++ esbuild.build.js
@@ -3,18 +3,6 @@
 require('esbuild').build({
     entryPoints: ["ts/list-renderer.ts"],
     bundle: true,
-    outfile: "dist/list-renderer.js",
-    minify: true,
-    sourcemap: true,
-    target: "es2020",
-    format: "cjs",
-    define: { "process.env.NODE_ENV": "production" },
-    // define: { "process.env.NODE_ENV": "developemnt" },
-})
-
-require('esbuild').build({
-    entryPoints: ["ts/list-renderer.ts"],
-    bundle: true,
     outfile: "dist/list-renderer.mjs",
     minify: true,
     sourcemap: true,
